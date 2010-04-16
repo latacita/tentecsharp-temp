31ed15e0e27a97eedaea4ffb3990623f36c12d2f
ealopezs <ealopezs@dda1ef0a-214d-2ac1-657b-0a6f9ea370a9>
2010-04-16 10:55:21 +0000

    Upload helpTentePlugin (aux)

---
+++ trunk/net.ample.wp2.helpTentePlugin/src/util/PageHelp.java
@@ -0,0 +1,9 @@
+package util;
+
+public class PageHelp {
+	
+	public static String TARGET_PROJECT_DOMAIN="/helptenteplugin.Activator/html/targetProjectDomain.html";
+	public static String TARGET_PROJECT_APPLICATION="/helptenteplugin.Activator/html/targetProjectApplication.html";
+	public static String REFERENCE_PROJECT_APPLICATION="/helptenteplugin.Activator/html/referenceProjectApplication.html";
+
+}
